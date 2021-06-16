82b2bbd68d37106851c37cfa23c1f2cf9c625421
Darrell Wright <beached@users.noreply.github.com>
2021-06-15 22:04:09 -0400

    Using checked iterator as range

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 // Official repository: https://github.com/beached/utfcpp_iostream
 //
 
-#include <utf8/unchecked.h>
+#include <utf8/checked.h>
 
 #include <cstdint>
 #include <fstream>
@@ -20,9 +20,8 @@ int main( int argc, char **argv ) {
 	}
 	auto first_stream = std::istreambuf_iterator<char>( in_file );
 	auto last_stream = std::istreambuf_iterator<char>( );
-	auto first = utf8::unchecked::iterator( first_stream );
-	auto last = utf8::unchecked::iterator( last_stream );
-	while( first != last ) {
-		std::cout << static_cast<std::uint32_t>( *first ) << '\n';
+	auto checked_range = utf8::iterator( first_stream, last_stream );
+	for( std::uint32_t cp : checked_range ) {
+    std::cout << static_cast<std::uint32_t>( cp ) << '\n';
 	}
 }
